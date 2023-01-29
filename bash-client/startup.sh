f80ec5bbb334d865c35907f7e77da65306543f81
dinuwan97 <kalubowila.dc25132@live.com>
2023-01-29 13:50:52 +0530

    bash updated

--- bash-client/startup.sh
+++ bash-client/startup.sh
@@ -3,7 +3,35 @@ while IFS="=" read -r key value; do
     eval "$key=$value"
 done < values.config
 
-echo "Customer Account : $customer_account_name" 
+# moving back to rrot directory
+cd ..
+
+# check if migration-docs from wso2-enterprise already avaialble
+if [ -d "migration-docs" ]; then
+  echo "migration-docs directory already available"
+else
+  echo "migration-docs directory is not available....."
+   echo "Do you want to clone migration docs from wso2-exterprise ? (Y/N)"
+   read cloning_option
+    if [ "$cloning_option" = "Y" ]; then
+      # clone migration docs from wso2-enterprise
+      echo "Accessing wso2-enterprise....."
+      git clone "https://$git_pat@github.com/wso2-enterprise/migration-docs.git"
+      echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------"
+      echo "---------------------------------------------------------------------Cloning Complted----------------------------------------------------------------------------"
+      echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------"
+    else
+      echo "Clone wso2-enterprise manually and perform the docs creation..."
+      echo "Use https://github.com/wso2-enterprise/migration-docs"
+    fi
+fi
+
+
+
+if [ -d "migration-docs" ]; then
+cd ./is-migration-docs-generator
+
+echo "Customer Account : $customer_account_name"
 echo "Source Version   : $source_is_version"
 echo "Target Version   : $target_is_version"
 # pick the directory name
@@ -11,151 +39,125 @@ directory_name="WSO2x$customer_account_name-Identity-Server-Migration-$source_is
 
 # move back to root
 cd ..
+
+if [ -d "$directory_name" ]; then
+  echo "Directory already exsists...."
+  echo "Migration docs creation gracefully stoped...."
+else
 # creating the customized migration doc directory
 mkdir "WSO2x$customer_account_name-Identity-Server-Migration-$source_is_version-$target_is_version"
 echo "Directory created : $directory_name"
 
 rootPath=$PWD
-echo "root path $rootPath"
 source_directory="$rootPath/migration-docs-main/identity-server/migration-clients"
-echo "source directory : $source_directory"
 destination_directory="$rooPath/$directory_name"
-echo "destination directory : $destination_directory"
 cd "$directory_name"
 mkdir "migration-client"
 
 
-#cp "./../migration-docs-main/identity-server/migration-clients/wso2is-migration-1.0.225.zip" "~/Desktop/IAM-Migration/Migration-Docs/WSO2xMobitel-Identity-Server-Migration-5.7.0-6.1.0/migration-client"
-#cp "./../migration-docs-main/identity-server/migration-clients/wso2is-migration-1.0.225.zip" "~/Desktop/IAM-Migration/Migration-Docs/WSO2xMobitel-Identity-Server-Migration-5.7.0-6.1.0/"
-
 #copy and paster migration clirnt zip
-cp -f "./../migration-docs-main/identity-server/migration-clients/wso2is-migration-$migration_cleint_veesion.zip" "$PWD/migration-client"
+cp -f "$migration_cleint_path/wso2is-migration-$migration_cleint_veesion.zip" "$PWD/migration-client"
+
 
 mkdir "migration-docs"
 cd "migration-docs"
 
+# creating sub directories of migration-docs directory
 mkdir "1-What-has-changed"
 mkdir "2-Migration-process"
 mkdir "3-Migration-docs"
 
+
+
+################################
+################################
+# moving into 1-What-has-changed
 cd "1-What-has-changed"
 
+# declared functions for what-has-changed process
+migrating_what_has_changed_57_to_58 () {
+  mkdir "5.7.0-to-5.8.0"
+  cd "5.7.0-to-5.8.0"
+  cp -f "$migration_docs_is_versions_wso2_enterprise/is-5.8.0/understanding-what-has-changed.md" "$PWD"
+  cd ..
+}
 
-echo "current directroy : $PWD"
+migrating_what_has_changed_58_to_59 () {
+  mkdir "5.8.0-to-5.9.0"
+  cd "5.8.0-to-5.9.0"
+  cp -f "$migration_docs_is_versions_wso2_enterprise/is-5.9.0/migrating-what-has-changed.md" "$PWD"
+  cd ..
+}
+
+migrating_what_has_changed_59_to_510 () {
+  mkdir "5.9.0-to-5.10.0"
+  cd "5.9.0-to-5.10.0"
+  cp -f "$migration_docs_is_versions_wso2_enterprise/is-5.10.0/migrating-what-has-changed.md" "$PWD"
+  cd ..
+}
+
+migrating_what_has_changed_510_to_511 () {
+  mkdir "5.10.0-to-5.11.0"
+  cd "5.10.0-to-5.11.0"
+  cp -f "$migration_docs_is_versions_wso2_enterprise/is-5.11.0/migrating-what-has-changed.md" "$PWD"
+  cd ..
+}
+
+migrating_what_has_changed_511_to_600 () {
+  mkdir "5.11.0-to-6.0.0"
+  cd "5.11.0-to-6.0.0"
+  cp -f "$migration_docs_is_versions_wso2_enterprise/is-6.0.0/what-has-changed.md" "$PWD"
+  cd ..
+}
 
 
 if [ "$source_is_version" = "5.7.0" ]
 then
-  mkdir "5.7.0-to-5.8.0"
-  cd "5.7.0-to-5.8.0"
-  cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-5.8.0/understanding-what-has-changed.md.md" "$PWD"
-  cd ..
-
-  mkdir "5.8.0-to-5.9.0"
-  cd "5.8.0-to-5.9.0"
-  cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-5.9.0/migrating-what-has-changed.md" "$PWD"
-  cd ..
-
-  mkdir "5.9.0-to-5.10.0"
-  cd "5.9.0-to-5.10.0"
-  cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-5.10.0/migrating-what-has-changed.md" "$PWD"
-  cd ..
-
+  migrating_what_has_changed_57_to_58
+  migrating_what_has_changed_58_to_59
+  migrating_what_has_changed_59_to_510
     if [ "$target_is_version" = "6.0.0" ]
     then
-      mkdir "5.10.0-to-5.11.0"
-      cd "5.10.0-to-5.11.0"
-      cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-5.11.0/migrating-what-has-changed.md" "$PWD"
-      cd ..
-
-      mkdir "5.11.0-to-6.0.0"
-      cd "5.11.0-to-6.0.0"
-      cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-6.0.0/what-has-changed.md" "$PWD"
-      cd ..
+      migrating_what_has_changed_510_to_511
+      migrating_what_has_changed_511_to_600
     else
-      mkdir "5.10.0-to-5.11.0"
-      cd "5.10.0-to-5.11.0"
-      cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-5.11.0/migrating-what-has-changed.md" "$PWD"
-      cd ..
+      migrating_what_has_changed_510_to_511
     fi
 elif [ "$source_is_version" = "5.8.0" ]
 then
-  mkdir "5.8.0-to-5.9.0"
-  cd "5.8.0-to-5.9.0"
-  cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-5.9.0/migrating-what-has-changed.md" "$PWD"
-  cd ..
-
-  mkdir "5.9.0-to-5.10.0"
-  cd "5.9.0-to-5.10.0"
-  cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-5.10.0/migrating-what-has-changed.md" "$PWD"
-  cd ..
+  migrating_what_has_changed_58_to_59
+  migrating_what_has_changed_59_to_510
     if [ "$target_is_version" = "6.0.0" ]
     then
-      mkdir "5.10.0-to-5.11.0"
-      cd "5.10.0-to-5.11.0"
-      cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-5.11.0/migrating-what-has-changed.md" "$PWD"
-      cd ..
-
-      mkdir "5.11.0-to-6.0.0"
-      cd "5.11.0-to-6.0.0"
-      cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-6.0.0/what-has-changed.md" "$PWD"
-      cd ..
+      migrating_what_has_changed_510_to_511
+      migrating_what_has_changed_511_to_600
     else
-      mkdir "5.10.0-to-5.11.0"
-      cd "5.10.0-to-5.11.0"
-      cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-5.11.0/migrating-what-has-changed.md" "$PWD"
-      cd ..
+      migrating_what_has_changed_510_to_511
     fi
 elif [ "$source_is_version" = "5.9.0" ]
 then
-  mkdir "5.9.0-to-5.10.0"
-  cd "5.9.0-to-5.10.0"
-  cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-5.10.0/migrating-what-has-changed.md" "$PWD"
-  cd ..
+  migrating_what_has_changed_59_to_510
     if [ "$target_is_version" = "6.0.0" ]
     then
-      mkdir "5.10.0-to-5.11.0"
-      cd "5.10.0-to-5.11.0"
-      cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-5.11.0/migrating-what-has-changed.md" "$PWD"
-      cd ..
-
-      mkdir "5.11.0-to-6.0.0"
-      cd "5.11.0-to-6.0.0"
-      cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-6.0.0/what-has-changed.md" "$PWD"
-      cd ..
+      migrating_what_has_changed_510_to_511
+      migrating_what_has_changed_511_to_600
     else
-      mkdir "5.10.0-to-5.11.0"
-      cd "5.10.0-to-5.11.0"
-      cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-5.11.0/migrating-what-has-changed.md" "$PWD"
-      cd ..
+      migrating_what_has_changed_510_to_511
     fi
 elif [ "$source_is_version" = "5.10.0" ]
 then
   if [ "$target_is_version" = "6.0.0" ]
   then
-      mkdir "5.10.0-to-5.11.0"
-      cd "5.10.0-to-5.11.0"
-      cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-5.11.0/migrating-what-has-changed.md" "$PWD"
-      cd ..
-
-      mkdir "5.11.0-to-6.0.0"
-      cd "5.11.0-to-6.0.0"
-      cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-6.0.0/what-has-changed.md" "$PWD"
-      cd ..
-    else
-      mkdir "5.10.0-to-5.11.0"
-      cd "5.10.0-to-5.11.0"
-      cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-5.11.0/migrating-what-has-changed.md" "$PWD"
-      cd ..
-    fi
+    migrating_what_has_changed_510_to_511
+    migrating_what_has_changed_511_to_600
+  else
+    migrating_what_has_changed_510_to_511
+  fi
 elif [ "$source_is_version" = "5.11.0" ]
 then
   if [ "$target_is_version" = "6.0.0" ]
   then
-    mkdir "5.11.0-to-6.0.0"
-    cd "5.11.0-to-6.0.0"
-    cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-6.0.0/what-has-changed.md" "$PWD"
-    cd ..
+    migrating_what_has_changed_511_to_600
   else
     echo "Target Version $target_is_version not supported"
   fi
@@ -163,15 +165,20 @@ else
  echo "Source Version $source_is_version not supported"
 fi
 
+echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "------------------------------------------------------------------What Has Changed step completed----------------------------------------------------------------"
+echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------"
+
 # exit from what-has-changed directory
 cd ..
 
-# moving into migration-process directory
+
+###########################################
+###########################################
+# moving into 2-Migration-process directory
 cd "2-Migration-process"
 
-echo "2-Migration-process : current directroy : $PWD"
 mkdir "$target_is_version"
-
 # moving into migration-process/target-is-version directory
 cd "$target_is_version"
 
@@ -179,44 +186,64 @@ mkdir "db-scripts"
 cd "db-scripts"
 mkdir "prepare-for-migration"
 cd "prepare-for-migration"
-
 # copying version cleaning scripts
-cp -a "./../../../../../../migration-docs-main/identity-server/migration-docs/is-5.10.0/migrating-preparing-for-migration/." "$PWD"
-
+cp -a "$version_cleaning_scripts_path" "$PWD"
 # moving back from is-5.10.0/migrating-preparing-for-migration/
 cd ./../../
-echo "$PWD"
 
 if [ "$target_is_version" = "5.11.0" ]
 then
- cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-5.11.0/migrating-preparing-for-migration.md" "$PWD"
- cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-5.11.0/migration-process.md" "$PWD"
+ cp -f "$migration_docs_is_versions_wso2_enterprise/is-5.11.0/migrating-preparing-for-migration.md" "$PWD"
+ cp -f "$migration_docs_is_versions_wso2_enterprise/is-5.11.0/migration-process.md" "$PWD"
 elif [ "$target_is_version" = "6.0.0" ]
 then
- cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-6.0.0/prepare-for-migration.md" "$PWD"
- cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-6.0.0/migration-process.md" "$PWD"
+ cp -f "$migration_docs_is_versions_wso2_enterprise/is-6.0.0/prepare-for-migration.md" "$PWD"
+ cp -f "$migration_docs_is_versions_wso2_enterprise/is-6.0.0/migration-process.md" "$PWD"
 else
  echo "2-Migration Process : Target Version $target_is_version not supported"
 fi
 
+echo "-----------------------------------------------------------------Migration Process step completed----------------------------------------------------------------"
+echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------"
+
 # moving back from /2-Migration-process/6.0.0
 cd ./../../
 
+
+########################################
+########################################
+# moving into 3-Migration-docs directory
 cd "3-Migration-docs"
 mkdir "$target_is_version"
 cd "$target_is_version"
 
 if [ "$target_is_version" = "5.11.0" ]
 then
- cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-5.11.0/migrating-to-5110.md" "$PWD"
+ cp -f "$migration_docs_is_versions_wso2_enterprise/is-5.11.0/migrating-to-5110.md" "$PWD"
 elif [ "$target_is_version" = "6.0.0" ]
 then
- cp -f "./../../../../migration-docs-main/identity-server/migration-docs/is-6.0.0/migrate-to-600.md" "$PWD"
+ cp -f "$migration_docs_is_versions_wso2_enterprise/is-6.0.0/migrate-to-600.md" "$PWD"
 else
  echo "3-Migration-docs : Target Version $target_is_version not supported"
 fi
 
-echo "$PWD"
+echo "-------------------------------------------------------------------Migration Docs step completed-----------------------------------------------------------------"
+echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------"
 
 cd ./../../../
-echo "$PWD"
+
+
+#########################
+#########################
+# Gracefully shuting down
+echo "Docs creation sucessfully completed."
+echo "Current directory : $PWD"
+# tree diagram of created migration doc directory
+tree -a
+fi
+
+# In user input was no, to clone the wso2-enterprise/migration-docs
+else
+  echo "WSO2 Identity Server Docs creation has forced shiutdown for $customer_account_name"
+  echo "Mannulay clone the wso2-enterprise/migration-docs and perform the docs creation again."
+fi
