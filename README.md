9ae83b48d3a43118c6077254b2f00392dd5af1cc
dinuwan97 <kalubowila.dc25132@live.com>
2023-01-29 20:00:36 +0530

    bash completed

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # is-migration-docs-generator
-This repository is exclusive for WSO2 IAM Customer Success Team
+##### This repository is exclusive for WSO2 IAM Customer Success Team
--- bash-client/startup.sh
+++ bash-client/startup.sh
@@ -3,6 +3,16 @@ while IFS="=" read -r key value; do
     eval "$key=$value"
 done < values.config
 
+
+if [ "$source_is_version" = "5.7.0" ] || [ "$source_is_version" = "5.8.0" ] || [ "$source_is_version" = "5.9.0" ] || [ "$source_is_version" = "5.10.0" ] || [ "$source_is_version" = "5.11.0" ]
+then
+ if [ "$target_is_version" = "5.11.0" ] || [ "$target_is_version" = "6.0.0" ]
+ then
+  if [ "$source_is_version" = "$target_is_version" ]
+  then
+   echo "Source and Target versions shoul not be same."
+  else
+     
 # moving back to rrot directory
 cd ..
 
@@ -36,7 +46,8 @@ echo "Source Version   : $source_is_version"
 echo "Target Version   : $target_is_version"
 # pick the directory name
 directory_name="WSO2x$customer_account_name-Identity-Server-Migration-$source_is_version-$target_is_version"
-
+directory_to_be_named="[WSO2 x $customer_account_name] Identity Server Migration from $source_is_version to $target_is_version"
+zip_file_to_be_name="$renamed_directory.zip"
 # move back to root
 cd ..
 
@@ -44,6 +55,13 @@ if [ -d "$directory_name" ]; then
   echo "Directory already exsists...."
   echo "Migration docs creation gracefully stoped...."
 else
+
+if [ -d "$directory_to_be_named" ]; then
+  echo "Directory already exsists...."
+  echo "Migration docs creation gracefully stoped...."
+else
+
+
 # creating the customized migration doc directory
 mkdir "WSO2x$customer_account_name-Identity-Server-Migration-$source_is_version-$target_is_version"
 echo "Directory created : $directory_name"
@@ -195,10 +213,23 @@ if [ "$target_is_version" = "5.11.0" ]
 then
  cp -f "$migration_docs_is_versions_wso2_enterprise/is-5.11.0/migrating-preparing-for-migration.md" "$PWD"
  cp -f "$migration_docs_is_versions_wso2_enterprise/is-5.11.0/migration-process.md" "$PWD"
+ cp -f "$migration_docs_is_versions_wso2_enterprise/is-5.11.0/migrating-userstore-managers.md" "$PWD"
+  if [ "$source_is_version" = "5.7.0" ] || [ "$source_is_version" = "5.8.0" ] || [ "$source_is_version" = "5.9.0" ]
+  then
+   cp -f "$migration_docs_is_versions_wso2_enterprise/is-5.10.0/migrating-to-log4j2.md" "$PWD"
+  else
+   echo "Source IS version : $source_is_version. Log4j2 Migration steps not required."
+  fi
 elif [ "$target_is_version" = "6.0.0" ]
 then
  cp -f "$migration_docs_is_versions_wso2_enterprise/is-6.0.0/prepare-for-migration.md" "$PWD"
  cp -f "$migration_docs_is_versions_wso2_enterprise/is-6.0.0/migration-process.md" "$PWD"
+   if [ "$source_is_version" = "5.7.0" ] || [ "$source_is_version" = "5.8.0" ] || [ "$source_is_version" = "5.9.0" ]
+  then
+   cp -f "$migration_docs_is_versions_wso2_enterprise/is-5.10.0/migrating-to-log4j2.md" "$PWD"
+  else
+   echo "Source IS version : $source_is_version. Log4j2 Migration steps not required."
+  fi
 else
  echo "2-Migration Process : Target Version $target_is_version not supported"
 fi
@@ -237,13 +268,66 @@ cd ./../../../
 #########################
 # Gracefully shuting down
 echo "Docs creation sucessfully completed."
-echo "Current directory : $PWD"
+
+# readme file creation
+touch README.md
+echo "READMME created."
+echo "## [WSO2 x $customer_account_name] Identity Server Migration from $source_is_version to $target_is_version" >> README.md
+current_time_gmt=$(date -u +"%Y-%m-%d %H:%M:%S")
+echo "\n #### Created at : $current_time_gmt GMT" >> README.md
+echo '\n ```' >> README.md
+tree -a >> README.md
+echo '```' >> README.md
+echo "README content updated."
+
+
+renamed_directory="[WSO2 x $customer_account_name] Identity Server Migration from $source_is_version to $target_is_version"
+echo "$renamed_directory"
+
 # tree diagram of created migration doc directory
 tree -a
+
+cd ..
+
+# Rename the directory
+mv "$directory_name" "$renamed_directory"
+echo "Directory renamed as : $renamed_directory"
+
+# zipping the created migration Docs
+zip_file_name="$renamed_directory.zip"
+
+if [ -d "$zip_file_name" ]; then
+  echo 'Zip file is already there.'
+else
+  zip -r "$zip_file_name" "$renamed_directory"
+  echo 'File zipped successfully.'
+  echo "$zip_file_name"
+  
+  # observing md5sum 
+  touch "$zip_file_name.log"
+  md5sum "$zip_file_name" >> "$zip_file_name.log"
+  
+  echo "md5sum value taken for the zip"
+  echo "$zip_file_name.log"
+fi
+
+# EOIF of the initial check of directory name
+fi
+# EOIF of the initial check of directory name, suppose to rename
 fi
 
 # In user input was no, to clone the wso2-enterprise/migration-docs
 else
   echo "WSO2 Identity Server Docs creation has forced shiutdown for $customer_account_name"
   echo "Mannulay clone the wso2-enterprise/migration-docs and perform the docs creation again."
+fi
+
+
+# EOIF of comparing to taregt with source
+fi
+  else
+    echo "$target_is_version is unsupported target version via migration-docs tool."
+  fi
+else
+ echo "$source_is_version is unsupported source version via migration-docs tool."
 fi
